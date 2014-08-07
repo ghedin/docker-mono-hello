e94a7957d6b1a2c54d1a07c5808e84de9e819a52
Fernando Ghedin <fernando.ghedin@gmail.com>
2014-08-06 22:46:59 -0300

    Hello world

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+#DOCKER-VERSION 0.9.1
+
+FROM    fghedin/docker-mono
+
+ADD	. /src
+
+RUN     mcs /src/hello.cs
+CMD	["mono", "/src/hello.exe"]
+
---
+++ hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MonoTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
+
